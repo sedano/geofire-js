174a5e2ff85c84f23a83c69c2cea655ddcdb339f
Alejandro Sedano <sedano@users.noreply.github.com>
2017-04-25 13:55:47 -0500

    Fix typo in errorHandler

--- examples/html5Geolocation/js/html5Geolocation.js
+++ examples/html5Geolocation/js/html5Geolocation.js
@@ -48,7 +48,7 @@
     } else if (error.code === 2) {
       log("Error: POSITION_UNAVAILABLE: Network is down or positioning satellites cannot be reached");
     } else if (error.code === 3) {
-      log("Error: TIMEOUT: Calculating the user's location too took long");
+      log("Error: TIMEOUT: Calculating the user's location took too long");
     } else {
       log("Unexpected error code")
     }
